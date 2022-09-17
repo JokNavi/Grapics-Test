c1b1d3110d8c2fe8cae1c44997ba85ddcf61d446
JokNavi <JokNavi1@gmail.com>
2022-09-17 15:36:27 +0200

    Structured RunGame

--- MainFiles/TextBasedGraphics/Testing/DemoGraphics.py
+++ MainFiles/TextBasedGraphics/Testing/DemoGraphics.py
@@ -4,7 +4,9 @@ class Graphics:
     # Creates the single lines lists
     def CreateLists():
         def CreateCurrentLine():
-            currentLine = ["> ", "A"]
+            currentLine = ["> "]
+            for item in range(20*3):
+                currentLine.append("-")
             return currentLine
 
         # Makes the input lists into one list
@@ -14,26 +16,33 @@ class Graphics:
             for line in range(20):
                 gameVisuals.append(currentLine)
             return gameVisuals
-        
-        #print the full list in seperate lines
-        def PrintLines(gameVisuals):
+
+        gameVisuals = ConcatenateFullList()
+        return gameVisuals
+
+    def RunGame(gameVisuals, printWhat):
+        # print the full list in seperate lines
+        def PrintLines():
             for lineNumber in range(20):
                 line = gameVisuals[lineNumber]
                 print(*line, sep="")
 
-        #print the full list in seperate numbered lines
-        def PrintNumberedLines(gameVisuals):
+        # print the full list in seperate numbered lines
+        def PrintNumberedLines():
             for lineNumber in range(20):
                 line = gameVisuals[lineNumber]
                 line = list(map(lambda x: x.replace('>', f"{str(lineNumber)}>"), line))
                 print(*line, sep="")
 
-        
-        gameVisuals = ConcatenateFullList()
-        PrintLines(gameVisuals)
-        #PrintNumberedLines(gameVisuals)
-    CreateLists()
+        if(printWhat == "Numbered"):
+            PrintNumberedLines()
+        elif(printWhat == "Default"):
+            PrintLines()
+        else: print("Error, non existent game type!")
 
+
+    gameVisuals = CreateLists()
+    RunGame(gameVisuals, "Default")
 
 
 Graphics()
