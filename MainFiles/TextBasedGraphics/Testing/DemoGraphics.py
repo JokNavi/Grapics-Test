619a63022545f49eca7f3ca3543c2f4a418a6835
JokNavi <JokNavi1@gmail.com>
2022-09-17 15:19:35 +0200

    Fixed syntax and some major logic/structure errors

--- MainFiles/TextBasedGraphics/Testing/DemoGraphics.py
+++ MainFiles/TextBasedGraphics/Testing/DemoGraphics.py
@@ -4,48 +4,36 @@ class Graphics:
     # Creates the single lines lists
     def CreateLists():
         def CreateCurrentLine():
-            currentLine = ["> ", "A", "NewLine"]
+            currentLine = ["> ", "A"]
             return currentLine
 
         # Makes the input lists into one list
-        def ConcatenateList(currentLine):
+        def ConcatenateFullList():
             gameVisuals = []
+            currentLine = CreateCurrentLine()
             for line in range(20):
-                for item in range(len(currentLine)):
-                    gameVisuals.append(currentLine[item])
+                gameVisuals.append(currentLine)
             return gameVisuals
-
-        # Used to find the index of certain characters.
-        # Only works on lists and only lists that don't have two of the sought after words next to eachother.
-        def findIndex(list, text):
-            positions = []
-            index = 0
-            if(list.index(text, index) > 0):
-                for line in range(20):
-                    index = list.index(text, index+1)
-                    positions.append(index)
-            else:
-                index = list.index(text, index)
-                positions.append(index)
-                for line in range(20-1):
-                    index = list.index(text, index+1)
-                    positions.append(index)
-            return positions
-
-        def PrintLines(gameVisuals, indexOfEndLine, indexOfStartLine):
+        
+        #print the full list in seperate lines
+        def PrintLines(gameVisuals):
             for lineNumber in range(20):
-                line = gameVisuals[indexOfStartLine[lineNumber]:indexOfEndLine[lineNumber]]
-                stringLine = ""
-                for item in line:
-                    stringLine = stringLine + item
-                print(stringLine)
+                line = gameVisuals[lineNumber]
+                print(*line, sep="")
 
-        currentLine = CreateCurrentLine()
-        gameVisuals = ConcatenateList(currentLine)
-        indexOfEndLine = findIndex(gameVisuals, "NewLine")
-        indexOfStartLine = findIndex(gameVisuals, "> ")
-        PrintLines(gameVisuals, indexOfEndLine, indexOfStartLine)
+        #print the full list in seperate numbered lines
+        def PrintNumberedLines(gameVisuals):
+            for lineNumber in range(20):
+                line = gameVisuals[lineNumber]
+                line = list(map(lambda x: x.replace('>', f"{str(lineNumber)}>"), line))
+                print(*line, sep="")
+
+        
+        gameVisuals = ConcatenateFullList()
+        PrintLines(gameVisuals)
+        #PrintNumberedLines(gameVisuals)
     CreateLists()
+
 
 
 Graphics()
