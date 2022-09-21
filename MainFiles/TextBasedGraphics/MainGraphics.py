d6b255f96bcb74dba44362eb36248a4166ca237e
JokNavi <JokNavi1@gmail.com>
2022-09-21 15:49:08 +0200

    Addded error handler

--- MainFiles/TextBasedGraphics/MainGraphics.py
+++ MainFiles/TextBasedGraphics/MainGraphics.py
@@ -2,6 +2,8 @@ import math
 import keyboard
 import time
 
+errorMesage = ""
+
 
 class Graphics:
     def __init__(self, screenSize, prefix, spacing, border):
@@ -37,9 +39,31 @@ class Graphics:
 
 
 class AddVisuals:
+    def __init__(self, SCREENSIZE):
+        self.YSCREENSIZE = SCREENSIZE
+        self.XSCREENSIZE = SCREENSIZE*3
+
+    def ShapeInputHandler(self, SCREENSIZE):
+        print("#ADD-SHAPE-MENU! \n")
+        print("- Shapes can be: Dot, Line")
+        print(f"- Size can be: 1 to {SCREENSIZE}")
+        print(f"- X Coords can be: 1 to {SCREENSIZE*3}\n")
+        print("Input your desired shape and size down below.")
+        return
+
+    def CheckBorder(self, GAMEVISUALS, LINE, slotValue):
+        hits = ["Hit" for _ in range(1) if LINE.index(slotValue) == 0 or LINE.index(
+            slotValue) == self.XSCREENSIZE or GAMEVISUALS.index(LINE) == 0 or GAMEVISUALS.index(LINE) == self.YSCREENSIZE]
+        if len(hits) > 0:
+            return True
+        else:
+            return False
+
     def IsEven(self, SIZE):
-        if(SIZE%2 == 0): return 0
-        else: return 1
+        if(SIZE % 2 == 0):
+            return 0
+        else:
+            return 1
 
     def AddDot(self, gameVisuals, X, Y):
         LINE = gameVisuals[Y]
@@ -48,12 +72,19 @@ class AddVisuals:
         return gameVisuals
 
     def AddLine(self, gameVisuals, SIZE, X, Y):
+        slotValue = "-"
         ISEVEN = AddVisuals.IsEven(self, SIZE)
         HALFSIZE = math.floor(SIZE/2)
         LINE = gameVisuals[Y]
-        LINE[(X-HALFSIZE):(X+HALFSIZE)+ISEVEN] = ["-" for _ in range(SIZE)]
-        gameVisuals[Y] = LINE
+        LINE[(X-HALFSIZE):(X+HALFSIZE)+ISEVEN] = [slotValue for _ in range(SIZE)]
+        if(AddVisuals.CheckBorder(self, gameVisuals, LINE, slotValue)):
+            global errorMesage
+            errorMesage = "Error. Cannot place shape there as it would overlap with the border."
+            gameVisuals = gameVisuals
+        else:
+            gameVisuals[Y] = LINE
         return gameVisuals
+
 
 class GameHandler:
 
@@ -65,24 +96,32 @@ class GameHandler:
         border = "#"
         return lower, screenSize, prefix, spacing, border
 
-    def GameLoop(self, GRAPHICS, gameVisuals, LOWER):
-        ADDVISUALS = AddVisuals()
+    def ErrorHandler():
+        global errorMesage
+        print(errorMesage)
+        errorMesage = ""
+
+    def GameLoop(self, GRAPHICS, gameVisuals, LOWER, SCREENSIZE):
+        ADDVISUALS = AddVisuals(SCREENSIZE)
         GRAPHICS.PlayFrame(gameVisuals)
         while True:
             time.sleep(0.05)
             if keyboard.is_pressed("esc"):
                 print("Program closing...")
                 return
-            if keyboard.is_pressed("space"):
+            elif keyboard.is_pressed("space"):
                 GRAPHICS.LowerFrame(LOWER)
                 GRAPHICS.PlayFrame(gameVisuals)
                 time.sleep(0.02)
-            if keyboard.is_pressed("a"):
-                gameVisuals = ADDVISUALS.AddLine(gameVisuals, 7, 10, 3)
-                gameVisuals = ADDVISUALS.AddDot(gameVisuals, 10, 3)
+            elif keyboard.is_pressed("a"):
+                # ADDVISUALS.ShapeInputHandler(SCREENSIZE)
+                gameVisuals = ADDVISUALS.AddLine(gameVisuals, 3, 1, 3)
                 GRAPHICS.LowerFrame(LOWER)
                 GRAPHICS.PlayFrame(gameVisuals)
                 time.sleep(0.02)
+            elif(len(errorMesage) > 0):
+                GameHandler.ErrorHandler()
+
 
 class InitiateProgram:
 
@@ -90,7 +129,7 @@ class InitiateProgram:
     LOWER, SCREENSIZE, PREFIX, SPACING, BORDER = GAMEHANDLER.GraphicsInputHandler()
     GRAPHICS = Graphics(SCREENSIZE, PREFIX, SPACING, BORDER)
     gameVisuals = GRAPHICS.CreateDefaultFrame()
-    GAMEHANDLER.GameLoop(GRAPHICS, gameVisuals, LOWER)
+    GAMEHANDLER.GameLoop(GRAPHICS, gameVisuals, LOWER, SCREENSIZE)
 
 
 InitiateProgram()
